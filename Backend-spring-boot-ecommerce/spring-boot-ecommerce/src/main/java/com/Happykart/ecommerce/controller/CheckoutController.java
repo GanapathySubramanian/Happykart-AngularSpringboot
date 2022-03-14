813f1c07f0afbefbc29e258de753c3bc943780bf
Ganapathy Subramanian S <60843507+GanapathySubramanian@users.noreply.github.com>
2022-03-14 22:17:49 +0530

    Frontend and backend

--- Backend-spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/Happykart/ecommerce/controller/CheckoutController.java
+++ Backend-spring-boot-ecommerce/spring-boot-ecommerce/src/main/java/com/Happykart/ecommerce/controller/CheckoutController.java
@@ -5,20 +5,23 @@ import com.Happykart.ecommerce.dto.PurchaseResponse;
 import com.Happykart.ecommerce.service.CheckoutService;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin("https://localhost:4200")
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
-    private final CheckoutService checkoutService;
 
-    public CheckoutController(CheckoutService checkoutService){
-        this.checkoutService=checkoutService;
+    private CheckoutService checkoutService;
+
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
     }
 
     @PostMapping("/purchase")
-    public PurchaseResponse placeorder(@RequestBody Purchase purchase){
-        PurchaseResponse purchaseResponse=checkoutService.placeOrder(purchase);
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
 
         return purchaseResponse;
     }
+
 }
